b0c83f6c331d865f4903e33baf5f294bdf2f8564
hg_yellow <jang010505@gmail.com>
2024-04-01 16:44:52 +0900

    feat: Add #5 SleepTime Decorator

--- src/webdriver/MyWebDriver.py
+++ src/webdriver/MyWebDriver.py
@@ -1,6 +1,12 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from utils.SleepTime import SleepTime
 
 class MyWebDriver:
     def __init__(self, driver: webdriver.Chrome, url: str) -> None:
@@ -15,9 +21,11 @@ class MyWebDriver:
     def run(self):
         self.driverOpen()
 
+    @SleepTime(2)
     def driverOpen(self) -> None:
         self._driver.get(self._url)
     
+    @SleepTime(2)
     def clickBtn(self, value_type: By, value: str) -> None:
         btn = self._driver.find_element(value_type, value)
         ActionChains(self._driver).click(btn).perform()
