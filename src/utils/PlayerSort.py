ea71d3f1924655420813eba4525f9e16e2a695e7
hg_yellow <jang010505@gmail.com>
2024-04-02 12:22:40 +0900

    feat: #16 타자의 타율, 안타, 홈런, 타수, 주루 정렬 기능 완성

--- src/utils/PlayerSort.py
+++ src/utils/PlayerSort.py
@@ -4,13 +4,18 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from domain.Hitter import Hitter
-from webdriver.crawling.HitterCrawler import HitterCrawler
+
+def sort_hitter_avg(hitter):
+    if hitter.getPA() < 20:
+        return -1
+    else:
+        return hitter.getAvg()
 
 def hitter_sort(hitters: list[Hitter], option: str, reverseType: bool=True, count: int=5):
     count = min(len(hitters), count)
 
     if option == 'AVG':
-        return sorted(hitters, key=Hitter.getAvg, reverse=reverseType)[:count]
+        return sorted(hitters, key=sort_hitter_avg, reverse=reverseType)[:count]
     if option == 'H':
         return sorted(hitters, key=Hitter.getHit, reverse=reverseType)[:count]
     if option == 'HR':
