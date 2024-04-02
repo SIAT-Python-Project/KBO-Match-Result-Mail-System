92de09c1e1a4b4fadcf0e38103ac6be73429fe1e
hg_yellow <jang010505@gmail.com>
2024-04-02 09:53:42 +0900

    refactor: Add PlayerCrawler.py

--- src/webdriver/crawling/PitcherCrawler.py
+++ src/webdriver/crawling/PitcherCrawler.py
@@ -1,73 +1,31 @@
 import os
 import sys
 
+from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from MyWebDriver import MyWebDriver
-from utils.SleepTime import SleepTime
+from PlayerCrawler import PlayerCrawler
 from domain.Pitcher import Pitcher
-from selenium.common.exceptions import NoSuchElementException
 
-class PitcherCrawler(MyWebDriver):
-    def __init__(self, driver: webdriver.Chrome, url: str) -> None:
-        super().__init__(driver, url)
+class PitcherCrawler(PlayerCrawler):
+    def __init__(self, driver: webdriver.Chrome, url: str, tableColNumbers: list[int]) -> None:
+        super().__init__(driver, url, tableColNumbers)
 
     @staticmethod
-    def of():
-        return PitcherCrawler(webdriver.Chrome(), 'https://www.koreabaseball.com/Record/Player/PitcherBasic/BasicOld.aspx')
-    
-    def run(self) -> list[Pitcher]:
-        self.driverOpen()
-        self.focus_display()
-        pitchers = self.get_pitcher_info()
+    def of() -> object:
+        driver = webdriver.Chrome()
+        url = 'https://www.koreabaseball.com/Record/Player/PitcherBasic/BasicOld.aspx'
+        tableColNumbers = [3, 4, 8, 19]
 
-        return pitchers
-    
-
-    @SleepTime(2)
-    def focus_display(self) -> None:
-        self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    def get_pitcher_info(self) -> list[Pitcher]:
-        page_count = self.get_page_count()
-
-        hitters = []
-
-        for count in range(1, page_count+1):
-            self.clickBtn(By.ID, f'cphContents_cphContents_cphContents_ucPager_btnNo{count%5 if count%5 != 0 else 5}')
-            
-            hitters += self.crawl_pitcher_info()
-
-            if count != page_count and count % 5 == 0:
-                self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnNext')
-
-        return hitters
-
-    def get_page_count(self) -> int:
-        try:
-            self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnLast')
-            
-            page_btns = self._driver.find_elements(By.CSS_SELECTOR, '#cphContents_cphContents_cphContents_udpContent > div.record_result > div > a')
-            count = int(page_btns[-2].text)
-            
-            self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnFirst')
-
-            return count
-        except NoSuchElementException:
-            return 1
-
-    def crawl_pitcher_info(self) -> list[Pitcher]:
-        rows = self._driver.find_elements(By.CSS_SELECTOR, '#cphContents_cphContents_cphContents_udpContent > div.record_result > table > tbody > tr')
+        return PitcherCrawler(driver, url, tableColNumbers)
         
-        return [self.create_pitcher(row) for row in rows]
-            
-    def create_pitcher(self, row):
-        name = row.find_element(By.CSS_SELECTOR, 'td:nth-child(2) > a').text
-        team = row.find_element(By.CSS_SELECTOR, 'td:nth-child(3)').text
-        earnedRunsAverage = row.find_element(By.CSS_SELECTOR, 'td:nth-child(4)').text
-        win = row.find_element(By.CSS_SELECTOR, 'td:nth-child(8)').text
-        strikeOuts = row.find_element(By.CSS_SELECTOR, 'td:nth-child(19)').text
+    def create_player(self, row) -> Pitcher:
+        player_info = [row.find_element(By.CSS_SELECTOR, 'td:nth-child(2) > a').text]
+        
+        for idx in self._tableColNumbers:
+            player_info.append(row.find_element(By.CSS_SELECTOR, f'td:nth-child({idx})').text)
 
-        return Pitcher(name, team, earnedRunsAverage, win, strikeOuts)
+        return Pitcher.of(*player_info)
---
+++ src/webdriver/crawling/PlayerCrawler.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from MyWebDriver import MyWebDriver
+from utils.SleepTime import SleepTime
+from selenium.common.exceptions import NoSuchElementException
+
+class PlayerCrawler(MyWebDriver):
+    def __init__(self, driver: webdriver.Chrome, url: str, tableColNumbers: list[int]) -> None:
+        super().__init__(driver, url)
+        self._tableColNumbers = tableColNumbers
+
+    def run(self) -> list[object]:
+        self.driverOpen()
+        self.focus_display()
+        players = self.get_player_info()
+
+        return players
+
+    @SleepTime(2)
+    def focus_display(self) -> None:
+        self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def get_player_info(self) -> list[object]:
+        page_count = self.get_page_count()
+
+        players = []
+
+        for count in range(1, page_count+1):
+            self.clickBtn(By.ID, f'cphContents_cphContents_cphContents_ucPager_btnNo{count%5 if count%5 != 0 else 5}')
+            
+            players += self.crawl_player_info()
+
+            if count != page_count and count % 5 == 0:
+                self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnNext')
+
+        return players
+
+    def get_page_count(self) -> int:
+        try:
+            self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnLast')
+            
+            page_btns = self._driver.find_elements(By.CSS_SELECTOR, '#cphContents_cphContents_cphContents_udpContent > div.record_result > div > a')
+            count = int(page_btns[-2].text)
+            
+            self.clickBtn(By.ID, 'cphContents_cphContents_cphContents_ucPager_btnFirst')
+
+            return count
+        except NoSuchElementException:
+            return 1
+
+    def crawl_player_info(self) -> list[object]:
+        rows = self._driver.find_elements(By.CSS_SELECTOR, '#cphContents_cphContents_cphContents_udpContent > div.record_result > table > tbody > tr')
+        
+        return [self.create_player(row) for row in rows]
+            
+    def create_player(self, row):
+        pass
