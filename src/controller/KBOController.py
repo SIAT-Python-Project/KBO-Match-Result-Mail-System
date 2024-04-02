2dcb865071f040c8adee1059cd4116d298fa594f
hg_yellow <jang010505@gmail.com>
2024-04-03 00:02:59 +0900

    feat: #26 KBOController에 크롤러모듈 연결 할 수 이ㅛ게 베이스 코드 작성

--- src/controller/KBOController.py
+++ src/controller/KBOController.py
@@ -5,24 +5,53 @@ class KBOController:
     def __init__(self, settings) -> None:
         self.__settings = settings
 
-    def run(self):
+    def run(self) -> str:
         email = self.__settings['email']
         mail = Mail(email['email'], email['passwd'])
 
         data = dict()
 
+        data['team_info'] = dict()
         data['ranking_info'] = dict()
+
+        for result in self.__settings['team']['result']:
+            data['team_info'][result] = self.select_result(result)
 
         for rank in self.__settings['rank']:
             data['ranking_info'][rank] = self.select_rank(rank)
 
-        print(data)
+        return self.to_HTML(data)
 
-    def select_rank(self, rank_type):
+    def select_result(self, result_type: str) -> list[object]:
+        if result_type == 'yester_info':
+            return 
+        if result_type == 'today_info':
+            return 
+        if result_type == 'yesterday_news':
+            return
+
+    def select_rank(self, rank_type: str) -> list[object]:
         if rank_type == 'team_rank':
             return 
         if rank_type == 'players_rank':
             return PlayerController.get_ranking_players()
+        
+
+    def to_HTML(self, data):
+        pass
+
+    def to_HTML_team_info(self, data):
+        pass
+
+    def to_HTML_recent_match(self, data):
+        pass
+
+    def to_HTML_today_match(self, data):
+        pass
+
+    def to_HTML_recent_match_news(self, data):
+        pass
+        
         
         
 
